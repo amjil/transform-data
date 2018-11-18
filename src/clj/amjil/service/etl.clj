5721cba20f820ab9dd2b6c5a04afd5ea9e19c254
amjil <amjilt@aliyun.com>
2018-11-18 18:44:24 +0800

    use query file s in sql.clj

--- src/clj/amjil/service/etl.clj
+++ src/clj/amjil/service/etl.clj
@@ -6,27 +6,16 @@
             [amjil.db :as db]
             [amjil.config :refer [env]]
             [amjil.util.etl :as etl]
+            [amjil.util.sql :as sql]
             [clj-time.format :as time-format]))
 
-
 (defn transformx [date]
-  (let [tables (:tables env)
-        tables (map #(last (str/split % #"\.")) tables)
-        table-in (str "('" (str/join "','" tables) "')")
-        sql-str (str "SEL a.TXDate as job_date ,a.JOB_BATCH   as  batch_id
-                                      , a.job_nm as job_nm, trim(b.DatabaseName) as dbname
-                                      FROM  PVIEW.VW_DSYNC_JOB_DONE a LEFT JOIN DBC.TABLES b
-                                      ON a.JOB_NM=b.TABLENAME
-                                      where SYNC_DT= ?
-                                      and trim(b.DatabaseName) in ('pdata', 'pmart', 'nmart', 'BASS1','DBODATA','KDDMART')
-                                      and a.JOB_NM in "
-                      table-in)
-        data (jdbc/query db/tdcore [sql-str date])]
+  (let [data (sql/query-td-job-log date)]
     (if (empty? data)
       (log/warn "The ETL  date = " date " has not done yet.......")
       (doall
         (for [d data]
-          (let [job-done (jdbc/query db/sqlite ["select * from job_logs where job_date = ? and job_nm = ?" (:job_date d) (:job_nm d)])
+          (let [job-done (sql/query-sqlite-job-log d)
                 date (str/replace (:job_date d) #"-" "")
                 table (str/lower-case (str (:dbname d) "." (:job_nm d)))]
             (log/warn d)
