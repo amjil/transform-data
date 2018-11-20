9030dcca53477b1564501a81ceba614a80a6c75d
amjil <amjilt@aliyun.com>
2018-11-20 15:45:05 +0800

    add etl-params

--- src/clj/amjil/util/etl.clj
+++ src/clj/amjil/util/etl.clj
@@ -3,17 +3,28 @@
             [amjil.util.fastexport :as fast]
             [amjil.util.import :as im]
             [amjil.util.sql :as sql]
+            [amjil.config :refer [env]]
             [clojure.tools.logging :as log]
             [clojure.java.io :as io]))
 
-(defn etl-transaction [date name]
+(defn import-type [name value]
+  (let [imtype (-> (get (:table-conf env) name) (get :itype))]
+    (if (empty? imtype)
+      value
+      imtype)))
+
+(defn etl-params [date name]
   (let [[out outtype in] (sql/table-name name)
         sql (sql/gen-sql out date)
         length-of-sql (count sql)
-        im-type (if (= 1 length-of-sql) 0 1)
+        im-type (import-type name (if (= 1 length-of-sql) 0 1))
         date (if (= 1 length-of-sql) date (last sql))
         filename (str "./data/" date "/" name ".txt")]
     (clojure.java.io/make-parents filename)
+    [outtype im-type filename date sql]))
+
+(defn etl-transaction [date name]
+  (let [[outtype im-type filename date sql] (etl-params date name)]
     (if (= 0 outtype)
       (export/unload-to-file filename sql)
       (fast/fast-export filename sql))
@@ -24,13 +35,7 @@
 
 
 (defn etl-export [date name]
-  (let [[out outtype in] (sql/table-name name)
-        sql (sql/gen-sql out date)
-        length-of-sql (count sql)
-        im-type (if (= 1 length-of-sql) 0 1)
-        date (if (= 1 length-of-sql) date (last sql))
-        filename (str "./data/" date "/" name ".txt")]
-    (clojure.java.io/make-parents filename)
+  (let [[outtype im-type filename date sql] (etl-params date name)]
     (if (= 0 outtype)
       (export/unload-to-file filename sql)
       (fast/fast-export filename sql))
